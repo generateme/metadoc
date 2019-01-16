29ca28c50babbd9c367f5fdb2b12e1f68b15fa67
genmeblog <generateme.blog@gmail.com>
2019-01-16 09:31:26 +0100

    upd

--- project.clj
+++ project.clj
@@ -4,14 +4,14 @@
   :license {:name "The Unlicence"
             :url "http://unlicense.org"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [zprint "0.4.9"]
+                 [zprint "0.4.10"]
                  [hiccup "1.0.5"]
                  [enlive "1.1.6"]
                  [org.pegdown/pegdown "1.6.0"]
                  [org.clojure/tools.namespace "0.2.11"]]
-  :profiles {:dev {:plugins [[refactor-nrepl "2.4.0-SNAPSHOT"]
-                             [cider/cider-nrepl "0.18.0-SNAPSHOT"]
-                             [lein-codox "0.10.3"]]
+  :profiles {:dev {:plugins [[refactor-nrepl "2.4.0"]
+                             [cider/cider-nrepl "0.19.0-SNAPSHOT"]
+                             [lein-codox "0.10.4"]]
                    :dependencies [[codox-theme-rdash "0.1.2"]]
                    :source-paths ["example"]
                    :codox {:themes [:rdash]

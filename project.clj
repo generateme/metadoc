0ce0a21e2dffe29ad243b7bcbccb53f9f93dc3c0
genme <generateme.blog@gmail.com>
2019-03-31 19:43:27 +0200

    deps bumped

--- project.clj
+++ project.clj
@@ -1,17 +1,17 @@
-(defproject metadoc "0.2.4"
+(defproject metadoc "0.2.5"
   :description "More documentation tags in metadata"
   :url "https://github.com/generateme/metadoc"
   :license {:name "The Unlicence"
             :url "http://unlicense.org"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [zprint "0.4.15"]
-                 [hiccup "2.0.0-alpha1"]
+                 [hiccup "2.0.0-alpha2"]
                  [enlive "1.1.6"]
                  [org.pegdown/pegdown "1.6.0"]
                  [org.clojure/tools.namespace "0.3.0-alpha4"]]
-  :profiles {:dev {:plugins [[refactor-nrepl "2.4.0"]
-                             [cider/cider-nrepl "0.20.0"]
-                             [lein-codox "0.10.5"]]
+  :profiles {:dev {:plugins [[refactor-nrepl "2.4.1-SNAPSHOT"]
+                             [cider/cider-nrepl "0.22.0-SNAPSHOT"]
+                             [lein-codox "0.10.6"]]
                    :dependencies [[codox-theme-rdash "0.1.2"]]
                    :source-paths ["example"]
                    :codox {:themes [:rdash]

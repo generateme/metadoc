9feb083b828e0d4283c42a71da31d1189304a929
GenerateMe <generateme.blog@gmail.com>
2021-08-20 20:24:57 +0200

    0.2.8 - deps bump

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 # Change Log
 All notable changes to this project will be documented in this file. This change log follows the conventions of [keepachangelog.com](http://keepachangelog.com/).
 
+## [0.2.8]
+
+Deps bump
+
 ## [0.2.7]
 
 ### Changed
--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject metadoc "0.2.7"
+(defproject metadoc "0.2.8"
   :description "More documentation tags in metadata"
   :url "https://github.com/generateme/metadoc"
   :license {:name "The Unlicence"
             :url "http://unlicense.org"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [zprint "0.4.16"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [zprint "1.1.1"]
                  [hiccup "2.0.0-alpha2"]
                  [enlive "1.1.6"]
                  [org.pegdown/pegdown "1.6.0"]
-                 [org.clojure/tools.namespace "0.3.1"]]
+                 [org.clojure/tools.namespace "1.1.0"]]
   :profiles {:dev-codox {:codox {:source-uri "https://github.com/generateme/metadoc/blob/master/{filepath}#L{line}"}}})
